a59a0cdb5fc95bff079f0f81065c6f2442021154
KOSTA-HP\KOSTA <chysub@gmail.com>
2017-06-13 10:27:49 +0900

    [영섭] 내가 12일날 한것들

--- src/main/java/kosta/web/controller/HomeController.java
+++ src/main/java/kosta/web/controller/HomeController.java
@@ -10,7 +10,6 @@ public class HomeController {
 	public String home() {
 		return "index";
 	}
-<<<<<<< HEAD
 	
 	@RequestMapping({"login/joinForm","{folder}/login/joinForm"})
 	public String joinForm(){
@@ -21,11 +20,9 @@ public class HomeController {
 	public String login(){
 		return "login/login";
 	}	
-=======
 
 	@RequestMapping("eating/eating_Main")
 	public String eatingMain(){
 		return "eating/eating_Main";
 	}
->>>>>>> 540031340cbae5b07c32476244439e0b56b30d43
 }
--- src/main/java/kosta/web/controller/food/FoodController.java
+++ src/main/java/kosta/web/controller/food/FoodController.java
@@ -1,13 +1,39 @@
 package kosta.web.controller.food;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kosta.web.model.service.food.RestaurantInfoService;
+import kosta.web.model.vo.restaurant.RestaurentVo;
+	
 
 @Controller
 public class FoodController {
+	@Autowired
+	private RestaurantInfoService restaurantInfoService;
+	
 	@RequestMapping("eating/eatingMain")
 	public String eatingMain(){
 		
 		return "eating/eatingMain";
+	}
+	
+	@RequestMapping("eating/test")
+	public ModelAndView Test( String contentCode){
+		System.out.println("123");
+
+		List<RestaurentVo> list = restaurantInfoService.RestauranSearch(null);
+		System.out.println(list);
+		
+
+		
+		return new ModelAndView("eating/test","list", list);
+		
 	}
 }
