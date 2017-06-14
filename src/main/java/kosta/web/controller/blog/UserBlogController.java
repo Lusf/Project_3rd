9f2d8981966d321f05e731174acd9d2df3b3b615
MSDN-SPECIAL\Administrator <qwea08217@gmail.com>
2017-06-14 18:54:47 +0900

    6.14 오후 6:54 - blog 대략 완료

--- src/main/java/kosta/web/controller/blog/UserBlogController.java
+++ src/main/java/kosta/web/controller/blog/UserBlogController.java
@@ -8,9 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import kosta.web.model.service.blog.UserBlogService;
+import kosta.web.model.service.user.UserService;
+import kosta.web.model.vo.UserVo;
 import kosta.web.model.vo.blog.UserBlogVo;
 
 @Controller
@@ -21,19 +25,35 @@ public class UserBlogController {
 	private UserBlogService blogService;
 	
 	@Autowired
+	private UserService userService;
+	
+	@Autowired
 	private HttpSession session;
 
 	@RequestMapping("/{id}")
 	public String userBlog(@PathVariable String id){
-		session.setAttribute("id", id);
+		UserVo user = userService.userSearchById(id);
+		session.setAttribute("blogId", id);
+		session.setAttribute("blogUserPic", user.getUserPic());
 		return "blog/blog";
 	}
 	
-	@RequestMapping("/selectTitle")
+	@RequestMapping(value = "/selectTitle", method = RequestMethod.POST)
 	@ResponseBody
-	public List<String> selectTitle(String category){
-		System.out.println("blogTitle: "+session.getAttribute("id")+", "+category);
+	public List<UserBlogVo> selectTitle(String category){
+		List<UserBlogVo> list = blogService.blogTitle((String)session.getAttribute("blogId"), category);
+		//System.out.println(list.get(0).getBlogTitle()+", "+list.get(0).getContentCode());
+		return list;
+	}
+	
+	@RequestMapping(value = "/selectCont", method = RequestMethod.POST)
+	@ResponseBody
+	public List<UserBlogVo> selectCont(String contentCode){
+		String id = (String)session.getAttribute("blogId");
 		
-		return blogService.blogTitle((String)session.getAttribute("id"), category);
+		List<UserBlogVo> list = blogService.selectCont(id, contentCode);
+		System.out.println(list.get(0).getBlogCont());
+		
+		return list;
 	}
 }
