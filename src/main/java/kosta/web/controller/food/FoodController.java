6a36a3e94a31cf93b5deb036f2c8933f9ac711f5
KOSTA-HP\KOSTA <chysub@gmail.com>
2017-06-21 15:28:10 +0900

    21일 영섭 점심커밋

--- src/main/java/kosta/web/controller/food/FoodController.java
+++ src/main/java/kosta/web/controller/food/FoodController.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,7 +18,6 @@ import kosta.web.model.service.food.RestaurantService;
 import kosta.web.model.vo.UserVo;
 import kosta.web.model.vo.blog.UserBlogVo;
 import kosta.web.model.vo.restaurant.RestaurantVo;
-import kosta.web.model.vo.travelge.TravelgeInfoVo;
 
 @Controller
 /*@RequestMapping("/eating")*/
